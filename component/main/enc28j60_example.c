88b62e33726ba050e352fe3945bba79a92d31849
edbueno19 <eduardabuenoe19@gmail.com>
2025-12-12 09:55:33 -0300

    Add files via upload

---
+++ component/main/enc28j60_example.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "esp_eth_enc28j60.h"
+#include "driver/spi_master.h"
+#include "enc28j60_init.h"
+
+void app_main(void){
+    ethernet_init();
+}
